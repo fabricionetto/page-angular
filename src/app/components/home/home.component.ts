c449a6389c5e2ad9833bd823e4b6561719725c29
fabricionetto <agostinhonetto73@gmail.com>
2024-04-12 10:09:37 -0300

    versao b

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,43 +1,46 @@
 import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { Headers } from '../../headers';
 import { Imagens } from '../../imagens';
-import { MenuHeaderComponent } from "../menu-header/menu-header.component";
-import { ScrollLeftComponent } from '../scroll-left/scroll-left.component';
 import { LocalImagensComponent } from '../local-imagens/local-imagens.component';
+
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [MenuHeaderComponent,ScrollLeftComponent, NgFor, CommonModule, LocalImagensComponent]
+    imports: [NgFor, CommonModule, LocalImagensComponent]
 })
 export class HomeComponent {
 
- 
-    imag: Imagens[]= [{
+    constructor(){}
+
+    imag: Imagens[]= [
+    {
         "id": 1,
         "imagem": "assets/casa-amarela.png",
         "nome": 'Casa Amarela',
         "endereco": 'Rua: Castro Silva',
         "bairro": 'sta. Ines',
-        "preco": 13.4779
+        "preco": 13.4779,
     },
+
     {
         "id": 2,
         "imagem": "assets/casa-azul.png",
         "nome": 'Casa Azul',
         "endereco": 'Av. Luiz Lemos',
         "bairro": 'sta. Lucia',
-        "preco": 134.778
+        "preco": 134.778,
     },
+
     {
         "id": 3,
-        "imagem": "/assets/casa-rosa.jpg",
-        "nome": 'Casa Rosa',
+        "imagem": "assets/casa-verde.png",
+        "nome": 'Casa Verde',
         "endereco": 'Rua: Maria Conceição',
-        "bairro": 'Madre Gertudres',
+        "bairro": 'sta. Lucia',
         "preco": 9.995
-    },]
+    },
+]
 }
